60b1e126ebc316a9ff1118cdad45b7b42df1af1b
marcelo.buongermino <marcelo.buongermino@NBQFC-7WHVYL3>
2023-02-12 23:22:54 -0300

    Adicionar arquivos de projeto.

---
+++ GamesCRUD/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+﻿using GamesCRUD.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Game>> ExibirTodosOsGames()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ GamesCRUD/Models/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace GamesCRUD.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Categoria { get; set; }
+        public string? DataLancamento { get; set; }
+    }
+}
